3f2f83190d32168166a542828837577ceee5e871
Nguyen Quang Anh <142569995+quanganh221@users.noreply.github.com>
2023-08-26 18:13:25 +0700

    Add files via upload

---
+++ nhapvaxuattrongc1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 10;
+	float b = 23.67892345;
+	char ch = 'A';
+	
+	printf("\nInteger data = %d", a);
+	printf("\nFloat data = %f", b);
+	printf("\nCharacter = %c", ch);
+	printf("\nThis prints the string");
+	printf("%s", "\nThis also prints a string");
+	
+	return 0;
+}
---
+++ nhapvaxuattrongc5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("aaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaa");
+	
+	return 0;
+}
