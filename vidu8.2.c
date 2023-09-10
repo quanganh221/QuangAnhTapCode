7cde8ebe9b90397dbf2af67ff9a769588b19c67b
Nguyen Quang Anh <142569995+quanganh221@users.noreply.github.com>
2023-09-10 22:36:57 +0700

    Add files via upload

---
+++ vidu8.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int Num1, Num2;
+	Num1 = 540;
+	Num2 = 243;
+	if (Num1 > Num2) {
+		printf("\nThe Greater Number Is: %d", Num1);
+	}	
+	else {
+		printf("\nThe Greater Number Is: %d", Num2);
+	}
+}
