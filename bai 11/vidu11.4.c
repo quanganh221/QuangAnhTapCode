7829af1889fcc467864e8a4012037a476fc5e78a
Nguyen Quang Anh <142569995+quanganh221@users.noreply.github.com>
2023-09-15 08:14:40 +0700

    Add files via upload

---
+++ bai 11/vidu11.4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main() {
+	char ary[5];
+	int i;
+	printf("\nEnter string: ");
+	scanf("%s", &ary);
+	printf("\nThe string is %s \n\n", ary);
+	
+	for (i = 0; i < 5; i++)
+	printf("\t%d", ary[i]);
+}
